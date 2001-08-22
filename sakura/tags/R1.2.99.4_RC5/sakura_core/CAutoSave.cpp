663c9e07c6528356d8d5b3831b8fa2dd0f344736
nobody <nobody@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2001-08-22 01:07:56 +0000

    This commit was manufactured by cvs2svn to create tag 'R1.2.99.4_RC5'.

---
+++ sakura/tags/R1.2.99.4_RC5/sakura_core/CAutoSave.cpp
@@ -0,0 +1,88 @@
+//	$Id$
+//
+//	CAutoSave.cpp
+/*! @file
+	ファイルの自動保存
+	@author genta
+	@date 2000
+	$Revision$
+*/
+/*
+	Copyright (C) 2000-2001, genta
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+#include "CAutoSave.h"
+
+//----------------------------------------------------------
+//	class CPassiveTimer
+//
+//----------------------------------------------------------
+/*!
+	時間間隔の設定
+	@param m 間隔(min)
+	間隔を0以下に設定したときは1秒とみなす。設定可能な最大間隔は35792分。
+*/
+void CPassiveTimer::SetInterval(int m)
+{
+	if( m <= 0 )
+		m = 1;
+	else if( m >= 35792 )	//	35792分以上だと int で表現できなくなる
+		m = 35792;
+
+	nInterval = m * MSec2Min;
+}
+/*!
+	タイマーの有効・無効の切り替え
+	@param flag true:有効 / false: 無効
+	無効→有効に切り替えたときはリセットされる。
+*/
+void CPassiveTimer::Enable(bool flag)
+{
+	if( bEnabled != flag ){	//	変更があるとき
+		bEnabled = flag;
+		if( flag ){	//	enabled
+			Reset();
+		}
+	}
+}
+/*!
+	外部で定期に実行されるところから呼び出される関数。
+	呼び出されると経過時間をチェックする。
+
+	@retval true 所定時間が経過した。このときは測定基準が自動的にリセットされる。
+	@retval false 所定の時間に達していない。
+*/
+bool CPassiveTimer::CheckAction(void)
+{
+	if( !IsEnabled() )	//	有効でなければ何もしない
+		return false;
+
+	//	時刻比較
+	DWORD now = ::GetTickCount();
+	int diff;
+
+	diff = now - nLastTick;	//	TickCountが一回りしてもこれでうまくいくはず...
+
+	if( diff < nInterval )	//	規定時間に達していない
+		return false;
+
+	Reset();
+	return true;
+}
+
+
+/*[EOF]*/
---
+++ sakura/tags/R1.2.99.4_RC5/sakura_core/CDicMgr.cpp
@@ -0,0 +1,178 @@
+//	$Id$
+/*!	@file
+	CDicMgrクラス
+
+	@author Norio Nakatani
+	@date	1998/11/05 作成
+	$Revision$
+*/
+/*
+	Copyright (C) 1998-2001, Norio Nakatani
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "CDicMgr.h"
+#include <stdio.h>
+#include "CRunningTimer.h"
+
+
+CDicMgr::CDicMgr()
+{
+	return;
+}
+
+
+
+
+CDicMgr::~CDicMgr()
+{
+	return;
+}
+
+
+
+
+/*
+||  キーワードの検索
+||
+||  最初に見つかったキーワードの意味を返す
+*/
+BOOL CDicMgr::Search( const char* pszKey, CMemory** ppcmemMean, const char* pszKeyWordHelpFile )
+{
+#ifdef _DEBUG
+	CRunningTimer cRunningTimer( (const char*)"CDicMgr::Search" );
+#endif
+//	char	szDir[_MAX_PATH];
+//	long	lPathLen;
+	long	i;
+	FILE*	pFile;
+	char	szLine[10240];
+	char*	pszDelimit = " /// ";
+	char*	pszWork;
+	int		nRes;
+	char*	pszToken;
+	char*	pszKeySeps = ",\0";
+
+
+	/* 辞書ファイル */
+	if( 0 >= lstrlen( pszKeyWordHelpFile ) ){
+		return FALSE;
+	}
+	pFile = fopen( pszKeyWordHelpFile, "r" );
+	if( NULL == pFile ){
+		return FALSE;
+	}
+	while( NULL != fgets( szLine, sizeof(szLine), pFile ) ){
+		pszWork = strstr( szLine, pszDelimit );
+		if( NULL != pszWork && szLine[0] != ';' ){
+			*pszWork = '\0';
+			pszWork += lstrlen( pszDelimit );
+
+			/* 最初のトークンを取得します。 */
+			pszToken = strtok( szLine, pszKeySeps );
+			while( NULL != pszToken ){
+				nRes = _stricmp( pszKey, pszToken );
+				if( 0 == nRes ){
+					for( i = 0; i < (int)lstrlen(pszWork); ++i ){
+						if( pszWork[i] == '\r' ||
+							pszWork[i] == '\n' ){
+							pszWork[i] = '\0';
+							break;
+						}
+					}
+					*ppcmemMean = new CMemory;
+//					(*ppcmemMean)->SetData( pszWork, lstrlen(pszWork) );
+					(*ppcmemMean)->SetDataSz( pszWork );
+					fclose( pFile );
+					return TRUE;
+				}
+				pszToken = strtok( NULL, pszKeySeps );
+			}
+		}
+	}
+	fclose( pFile );
+	return FALSE;
+}
+
+
+
+
+
+/*
+||  入力補完キーワードの検索
+||
+||  ・見つかった候補をすべて返す(改行で区切って返す)
+||  ・指定された候補の最大数を超えると処理を中断する
+||  ・見つかった数を返す
+||
+*/
+int CDicMgr::HokanSearch(
+			const char* pszKey,
+			BOOL		bHokanLoHiCase,	/*英大文字小文字を同一視する*/
+			CMemory**	ppcmemKouho,
+			int			nMaxKouho,	//Max候補数(0==無制限)
+			const char* pszKeyWordFile
+)
+{
+	FILE*	pFile;
+	char	szLine[1024];
+	int		nKeyLen;
+	int		nKouhoNum;
+	int		nRet;
+	if( 0 >= lstrlen( pszKeyWordFile ) ){
+		return 0;
+	}
+	pFile = fopen( pszKeyWordFile, "r" );
+	if( NULL == pFile ){
+		return 0;
+	}
+	nKouhoNum = 0;
+	nKeyLen = lstrlen( pszKey );
+	*ppcmemKouho = NULL;
+	while( NULL != fgets( szLine, sizeof(szLine), pFile ) ){
+		if( nKeyLen > (int)lstrlen( szLine ) ){
+			continue;
+		}
+		if( szLine[0] == ';' ){
+			continue;
+		}
+		if( szLine[nKeyLen] == '\r' || szLine[nKeyLen] == '\n' ){
+			continue;
+		}
+		if( bHokanLoHiCase ){	/* 英大文字小文字を同一視する */
+			nRet = memicmp( pszKey, szLine, nKeyLen );
+		}else{
+			nRet = memcmp( pszKey, szLine, nKeyLen );
+		}
+		if( 0 == nRet ){
+			if( NULL == *ppcmemKouho ){
+				*ppcmemKouho = new CMemory;
+				(*ppcmemKouho)->SetDataSz( szLine );
+			}else{
+				(*ppcmemKouho)->AppendSz( szLine );
+			}
+			++nKouhoNum;
+			if( 0 != nMaxKouho && nMaxKouho <= nKouhoNum ){
+				break;
+			}
+		}
+	}
+	fclose( pFile );
+	return nKouhoNum;
+}
+
+
+/*[EOF]*/
