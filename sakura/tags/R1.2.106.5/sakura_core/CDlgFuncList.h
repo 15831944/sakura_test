f11223f27147370b224a57912a494a1f35e27885
nobody <nobody@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2002-03-07 15:40:28 +0000

    This commit was manufactured by cvs2svn to create tag 'R1.2.106.5'.

---
+++ sakura/tags/R1.2.106.5/sakura_core/CDlgFuncList.h
@@ -0,0 +1,94 @@
+//	$Id$
+/*!	@file
+	@brief アウトライン解析ダイアログボックス
+
+	@author Norio Nakatani
+	@date 1998/06/23 新規作成
+	@date 1998/12/04 再作成
+	$Revision$
+*/
+/*
+	Copyright (C) 1998-2001, Norio Nakatani
+	Copyright (C) 2001, genta, hor
+	Copyright (C) 2002, aroka
+
+	This source code is designed for sakura editor.
+	Please contact the copyright holder to use this code for other purpose.
+*/
+
+class CDlgFuncList;
+
+#ifndef _CDLGFUNCLIST_H_
+#define _CDLGFUNCLIST_H_
+
+#include <windows.h>
+#include "CDialog.h"
+//#include "CFuncInfoArr.h" // 2002/2/10 aroka ヘッダ整理
+//#include "CShareData.h" // 2002/2/10 aroka
+class CFuncInfoArr; // 2002/2/10 aroka
+
+
+//!	アウトライン解析ダイアログボックス
+class SAKURA_CORE_API CDlgFuncList : public CDialog
+{
+public:
+	/*
+	||  Constructors
+	*/
+	CDlgFuncList();
+	/*
+	||  Attributes & Operations
+	*/
+	HWND DoModeless( HINSTANCE, HWND, LPARAM, CFuncInfoArr*, int, int, int );/* モードレスダイアログの表示 */
+	void ChangeView( LPARAM );	/* モードレス時：検索対象となるビューの変更 */
+
+	CFuncInfoArr*	m_pcFuncInfoArr;	/* 関数情報配列 */
+	int				m_nCurLine;			/* 現在行 */
+	int				m_nSortCol;			/* ソートする列番号 */
+	int				m_nListType;		/* 一覧の種類 */
+	CMemory			m_cmemClipText;		/* クリップボードコピー用テキスト */
+	int				m_bLineNumIsCRLF;	/* 行番号の表示 FALSE=折り返し単位／TRUE=改行単位 */
+protected:
+	BOOL OnInitDialog( HWND, WPARAM, LPARAM );
+	BOOL OnBnClicked( int );
+	BOOL OnNotify( WPARAM, LPARAM );
+	BOOL OnSize( WPARAM, LPARAM );
+	void SetData( void );	/* ダイアログデータの設定 */
+	int GetData( void );	/* ダイアログデータの取得 */
+
+	/*
+	||  実装ヘルパ関数
+	*/
+	BOOL OnJump( bool bCheckAutoClose = true );	//	bCheckAutoClose：「このダイアログを自動的に閉じる」をチェックするかどうか
+	void SetTreeCpp( HWND );	/* ツリーコントロールの初期化：C++メソッドツリー */
+	void SetTreeJava( HWND, BOOL );	/* ツリーコントロールの初期化：Javaメソッドツリー */
+	void SetTreeTxt( HWND );	/* ツリーコントロールの初期化：テキストトピックツリー */
+	int SetTreeTxtNest( HWND, HTREEITEM, int, int, HTREEITEM*, int );
+	void GetTreeTextNext( HWND, HTREEITEM, int );
+
+	// 2001.12.03 hor
+//	void SetTreeBookMark( HWND );		/* ツリーコントロールの初期化：ブックマーク */
+	LPVOID GetHelpIdTable(void);	//@@@ 2002.01.18 add
+	void Key2Command( WORD );		//	キー操作→コマンド変換
+
+private:
+	//	May 18, 2001 genta
+	/*!
+		@brief アウトライン解析種別
+
+		0: List, 1: Tree
+	*/
+	int	m_nViewType;
+
+	// 2002.02.16 hor Treeのダブルクリックでフォーカス移動できるように 1/4
+	// (無理矢理なのでどなたか修正お願いします)
+	bool m_bWaitTreeProcess;
+};
+
+
+
+///////////////////////////////////////////////////////////////////////
+#endif /* _CDLGFUNCLIST_H_ */
+
+
+/*[EOF]*/
---
+++ sakura/tags/R1.2.106.5/sakura_core/CFuncInfo.cpp
@@ -0,0 +1,50 @@
+//	$Id$
+/*!	@file
+	@brief アウトライン解析  データ要素
+
+	@author Norio Nakatani
+	@date	1998/06/23 作成
+	$Revision$
+*/
+/*
+	Copyright (C) 1998-2001, Norio Nakatani
+
+	This source code is designed for sakura editor.
+	Please contact the copyright holder to use this code for other purpose.
+*/
+
+#include "CFuncInfo.h"
+//#include <windows.h> // 2002/2/10 aroka
+//#include <string.h>
+
+/*! CFuncInfoクラス構築 */
+CFuncInfo::CFuncInfo(
+	int		nFuncLineCRLF,	/*!< 関数のある行(CRLF単位) */
+	int		nFuncLineLAYOUT,/*!< 関数のある行(折り返し単位) */
+//	int		nFuncLine,		/*!< 関数のある行 */
+	char*	pszFuncName,	/*!< 関数名 */
+	int		nInfo			/*!< 付加情報 */
+)
+{
+	m_nFuncLineCRLF = nFuncLineCRLF;		/* 関数のある行(CRLF単位) */
+	m_nFuncLineLAYOUT = nFuncLineLAYOUT;	/* 関数のある行(折り返し単位) */
+//	m_nFuncLine = nFuncLine;;
+//	m_cmemFuncName = pszFuncName;
+//	m_cmemFuncName.SetData( pszFuncName, lstrlen( pszFuncName ) );
+	m_cmemFuncName.SetDataSz( pszFuncName );
+
+	m_nInfo = nInfo;
+	return;
+}
+
+
+
+
+/* CFuncInfoクラス消滅 */
+CFuncInfo::~CFuncInfo()
+{
+
+}
+
+
+/*[EOF]*/
