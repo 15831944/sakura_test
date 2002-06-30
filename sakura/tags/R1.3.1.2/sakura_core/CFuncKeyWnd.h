ad679c3cd1fc130e07e465c5f4fdc75d67a868f9
nobody <nobody@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2002-06-30 07:53:51 +0000

    This commit was manufactured by cvs2svn to create tag 'R1.3.1.2'.

---
+++ sakura/tags/R1.3.1.2/sakura_core/CFuncKeyWnd.h
@@ -0,0 +1,77 @@
+//	$Id$
+/*!	@file
+	@brief ファンクションキーウィンドウ
+
+	@author Norio Nakatani
+	$Revision$
+*/
+/*
+	Copyright (C) 1998-2001, Norio Nakatani
+
+	This source code is designed for sakura editor.
+	Please contact the copyright holder to use this code for other purpose.
+*/
+class CFuncKeyWnd;
+
+#ifndef _CFUNCKEYWND_H_
+#define _CFUNCKEYWND_H_
+
+#include "CWnd.h"
+//#include <windows.h>
+#include "CShareData.h"
+//#include "CEditDoc.h" // 2002/2/10 aroka
+class CEditDoc; // 2002/2/10 aroka
+
+//! ファンクションキーウィンドウ
+//	@date 2002.2.17 YAZAKI CShareDataのインスタンスは、CProcessにひとつあるのみ。
+class SAKURA_CORE_API CFuncKeyWnd : public CWnd
+{
+public:
+	/*
+	||  Constructors
+	*/
+	CFuncKeyWnd();
+	virtual ~CFuncKeyWnd();
+	/*
+	|| メンバ関数
+	*/
+	HWND Open( HINSTANCE, HWND, CEditDoc*, BOOL );	/* ウィンドウ オープン */
+	void Close( void );	/* ウィンドウ クローズ */
+	void SizeBox_ONOFF( BOOL );	/* サイズボックスの表示／非表示切り替え */
+	/*
+	|| メンバ変数
+	*/
+	const char*		m_pszClassName;	/*!< クラス名 */
+	DLLSHAREDATA*	m_pShareData;
+	CEditDoc*		m_pCEditDoc;
+	HFONT			m_hFont;	/*!< 表示用フォント */
+	int				m_nCurrentKeyState;
+	char			m_szFuncNameArr[12][256];
+	HWND			m_hwndButtonArr[12];
+	int				m_nFuncCodeArr[12];
+	int				m_nButtonGroupNum;
+	BOOL			m_bSizeBox;
+	HWND			m_hwndSizeBox;
+	int				m_nTimerCount;
+protected:
+	/*
+	|| 実装ヘルパ系
+	*/
+	void CreateButtons( void );	/* ボタンの生成 */
+	int CalcButtonSize( void );	/* ボタンのサイズを計算 */
+
+	/* 仮想関数 */
+
+	/* 仮想関数 メッセージ処理 詳しくは実装を参照 */
+	virtual LRESULT OnTimer( HWND, UINT, WPARAM, LPARAM );	// WM_TIMERタイマーの処理
+	virtual LRESULT OnCommand( HWND, UINT, WPARAM, LPARAM );	// WM_COMMAND処理
+	virtual LRESULT OnSize( HWND, UINT, WPARAM, LPARAM );// WM_SIZE処理
+	virtual LRESULT OnDestroy( HWND, UINT, WPARAM, LPARAM );// WM_DSESTROY処理
+};
+
+
+///////////////////////////////////////////////////////////////////////
+#endif /* _CFUNCKEYWND_H_ */
+
+
+/*[EOF]*/
