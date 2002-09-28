abe59713ecc815a138103fc018ec7f0d26d56dbd
nobody <nobody@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2002-09-28 18:14:19 +0000

    This commit was manufactured by cvs2svn to create tag 'R1.3.4.2'.

---
+++ sakura/tags/R1.3.4.2/sakura_core/CPropComUrl.cpp
@@ -0,0 +1,150 @@
+//	$Id$
+/*!	@file
+	@brief 共通設定ダイアログボックス、「クリッカブルURL」ページ
+
+	@author Norio Nakatani
+	$Revision$
+*/
+/*
+	Copyright (C) 1998-2001, Norio Nakatani
+
+	This source code is designed for sakura editor.
+	Please contact the copyright holder to use this code for other purpose.
+*/
+
+#include "CPropCommon.h"
+
+//@@@ 2001.02.04 Start by MIK: Popup Help
+#if 1	//@@@ 2002.01.03 add MIK
+#include "sakura.hh"
+static const DWORD p_helpids[] = {	//11100
+	IDC_CHECK_bSelectClickedURL,	HIDC_CHECK_bSelectClickedURL,	//クリッカブルURL
+//	IDC_STATIC,						-1,
+	0, 0
+};
+#else
+static const DWORD p_helpids[] = {	//11100
+	IDC_CHECK_bSelectClickedURL,	11110,	//クリッカブルURL
+//	IDC_STATIC,						-1,
+	0, 0
+};
+#endif
+//@@@ 2001.02.04 End
+
+//	From Here Jun. 2, 2001 genta
+/*!
+	@param hwndDlg ダイアログボックスのWindow Handle
+	@param uMsg メッセージ
+	@param wParam パラメータ1
+	@param lParam パラメータ2
+*/
+BOOL CALLBACK CPropCommon::DlgProc_PROP_URL(
+	HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam )
+{
+	return DlgProc( DispatchEvent_PROP_URL, hwndDlg, uMsg, wParam, lParam );
+}
+//	To Here Jun. 2, 2001 genta
+
+/* メッセージ処理 */
+BOOL CPropCommon::DispatchEvent_PROP_URL( HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam )
+{
+//	WORD		wNotifyCode;
+//	WORD		wID;
+//	HWND		hwndCtl;
+	NMHDR*		pNMHDR;
+	NM_UPDOWN*	pMNUD;
+	int			idCtrl;
+//	int			nVal;
+//	LPDRAWITEMSTRUCT pDis;
+
+	switch( uMsg ){
+
+	case WM_INITDIALOG:
+		/* ダイアログデータの設定 p1 */
+		SetData_PROP_URL( hwndDlg );
+		::SetWindowLong( hwndDlg, DWL_USER, (LONG)lParam );
+
+		/* ユーザーがエディット コントロールに入力できるテキストの長さを制限する */
+
+		return TRUE;
+	case WM_NOTIFY:
+		idCtrl = (int)wParam;
+		pNMHDR = (NMHDR*)lParam;
+		pMNUD  = (NM_UPDOWN*)lParam;
+//		switch( idCtrl ){
+//		default:
+			switch( pNMHDR->code ){
+			case PSN_HELP:
+				OnHelp( hwndDlg, IDD_PROP_URL );
+				return TRUE;
+			case PSN_KILLACTIVE:
+				/* ダイアログデータの取得 p1 */
+				GetData_PROP_URL( hwndDlg );
+				return TRUE;
+//@@@ 2002.01.03 YAZAKI 最後に表示していたシートを正しく覚えていないバグ修正
+			case PSN_SETACTIVE:
+				m_nPageNum = ID_PAGENUM_URL;
+				return TRUE;
+			}
+//			break;	/* default */
+//		}
+		break;	/* WM_NOTIFY */
+
+//@@@ 2001.02.04 Start by MIK: Popup Help
+	case WM_HELP:
+		{
+			HELPINFO *p = (HELPINFO *)lParam;
+			::WinHelp( (HWND)p->hItemHandle, m_szHelpFile, HELP_WM_HELP, (DWORD)(LPVOID)p_helpids );
+		}
+		return TRUE;
+		/*NOTREACHED*/
+		//break;
+//@@@ 2001.02.04 End
+
+//@@@ 2001.12.22 Start by MIK: Context Menu Help
+	//Context Menu
+	case WM_CONTEXTMENU:
+		::WinHelp( hwndDlg, m_szHelpFile, HELP_CONTEXTMENU, (DWORD)(LPVOID)p_helpids );
+		return TRUE;
+//@@@ 2001.12.22 End
+
+	}
+	return FALSE;
+}
+
+
+/* ダイアログデータの設定 */
+void CPropCommon::SetData_PROP_URL( HWND hwndDlg )
+{
+//	BOOL	bRet;
+
+//	BOOL	m_bSelectClickedURL;	/* URLがクリックされたら選択するか */
+
+
+	/* URLがクリックされたら選択するか */
+	::CheckDlgButton( hwndDlg, IDC_CHECK_bSelectClickedURL, m_Common.m_bSelectClickedURL );
+
+	return;
+}
+
+
+
+
+
+/* ダイアログデータの取得 */
+int CPropCommon::GetData_PROP_URL( HWND hwndDlg )
+{
+//@@@ 2002.01.03 YAZAKI 最後に表示していたシートを正しく覚えていないバグ修正
+//	m_nPageNum = ID_PAGENUM_URL;
+
+
+
+	/* URLがクリックされたら選択するか */
+	m_Common.m_bSelectClickedURL = ::IsDlgButtonChecked( hwndDlg, IDC_CHECK_bSelectClickedURL );
+
+
+	return TRUE;
+}
+
+
+/*[EOF]*/
