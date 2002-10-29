6348c9c7fe8e0166dfd8ce0b297d7fc48c62596b
nobody <nobody@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2002-10-29 17:47:00 +0000

    This commit was manufactured by cvs2svn to create tag 'R1.3.5.5'.

---
+++ sakura/tags/R1.3.5.5/sakura_core/CWaitCursor.cpp
@@ -0,0 +1,41 @@
+//	$Id$
+/*!	@file
+	@brief 砂時計カーソル
+
+	@author Norio Nakatani
+	$Revision$
+*/
+/*
+	Copyright (C) 1998-2001, Norio Nakatani
+
+	This source code is designed for sakura editor.
+	Please contact the copyright holder to use this code for other purpose.
+*/
+
+#include "CWaitCursor.h"
+
+/*!
+	現在のカーソルを保存し、カーソルを砂時計にする
+*/
+CWaitCursor::CWaitCursor( HWND hWnd )
+{
+	SetCapture( hWnd );
+	m_hCursor = ::LoadCursor( NULL, IDC_WAIT );
+	m_hCursorOld = ::SetCursor( m_hCursor );
+	return;
+}
+
+
+
+/*!
+	カーソル形状を元に戻す
+*/
+CWaitCursor::~CWaitCursor()
+{
+	ReleaseCapture();
+	::SetCursor( m_hCursorOld );
+	return;
+}
+
+
+/*[EOF]*/
